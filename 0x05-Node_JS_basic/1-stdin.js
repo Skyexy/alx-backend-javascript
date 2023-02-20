c768a22a51cf080e2e5c1a5a0354f6055bbe35d4
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-20 15:34:35 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,11 @@
-const prompt = require('prompt-sync')({sigint: true});
-const name = prompt("Welcome to Holberton School, what is your name?\n");
-console.log(`Your name is: ${name}`);
-process.on('exit', function(code) {
-  return console.log("This important software is now closing");
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+readline.question("Welcome to Holberton School, what is your name?\n", name => {
+  console.log(`Your name is: ${name}`);
+  readline.close();
+  process.on('exit', function(code) {
+    return console.log("This important software is now closing");
+  });
 });
