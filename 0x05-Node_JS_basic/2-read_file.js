ea47de4a8dc073a560765abdd29cab1720e8881b
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-20 16:20:45 +0100

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 module.exports = function countStudents(){
   fs.readFile(arguments[0], "utf-8", (err, data) => {
     if (err) process.stdout.write("Cannot load the database");
-    else console.log(data.firstname);
+    else console.log(data.lenght);
   });
 }
