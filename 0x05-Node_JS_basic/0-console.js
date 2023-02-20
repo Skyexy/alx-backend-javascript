7e0acaa0dbe685b0ed511e841ff1c7e6d22f039b
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-20 14:48:48 +0100

    Create 0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+function displayMessage(){
+    for (let i = 0; i < arguments.length; i++)
+    {
+        if (typeof arguments[i] == 'string')
+        {
+            process.stdout.write(arguments[i]);
+        }
+    }
+}
