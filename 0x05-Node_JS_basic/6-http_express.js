7f33c59389a6bb26c29e7e010380e36a9f4c7236
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-21 00:00:09 +0100

    Update 6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,5 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   res.end('Hello World');
-}).listen(1245)
+   res.send('Hello World');
+})
+
+app.listen(1245);
+module.exports = app;
