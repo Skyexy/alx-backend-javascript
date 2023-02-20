2057996420e4b4884485787799fb519c0649b498
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-20 23:29:22 +0100

    Create 5-http.js

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,27 @@
+var http = require('http');
+const countStudents = require('./3-read_file_async');
+const args = process.argv.slice(2);
+
+const app = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  if (req.url == '/') {
+    res.write('Hello Holberton School!');
+  }else if (url === '/students'){
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(arg[0]);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.statusCode = 404;
+      res.end(error.message);
+    }
+  }
+  res.end();
+}).listen(1245);
+
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
+
+});
+
+module.exports = app;
