b3612151f8993f8b2c9991e7deed34539036d489
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-20 22:28:11 +0100

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,22 @@
-const fs = require("fs");
-module.exports = function countStudents(){
-  var fs = require("fs");
-  let data = fs.readFileSync(arguments[0], 'utf8').toString().split('\n');
-  data = data.slice(1, data.length - 1);
-  process.stdout.write(`Number of students: ${data.length}`);
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    let data = fs.readFileSync(path, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const obj = {};
+    data.forEach((el) => {
+      const student = el.split(',');
+      if (!obj[student[3]]) obj[student[3]] = [];
+      obj[student[3]].push(student[0]);
+    });
+    for (const cls in obj) {
+      if (cls) console.log(`Number of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
+
+module.exports = countStudents;
