753d954e979314f59eaf799e5b1d169fb7eded16
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-20 16:49:02 +0100

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,5 +2,6 @@ const fs = require("fs");
 module.exports = function countStudents(){
   var fs = require("fs");
   let data = fs.readFileSync(arguments[0], 'utf8').toString().split('\n');
+  data = data.slice(1, data.length - 1);
   console.log(data)
 }
