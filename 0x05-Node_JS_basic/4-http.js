49bffb7b39f69e450df27d432f6c649db2ef9db3
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-21 22:23:53 +0100

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,11 @@
-var http = require('http');
+const http = require('http');
 
-const app = http.createServer(function (req, res) {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-}).listen(1245);
+});
+
+app.listen(1245);
 
 module.exports = app;
