ea1a9d3adcb47b4dc99a3188bb0d6d2b56755010
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-24 23:25:15 +0100

    Update 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,13 +3,13 @@ const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber", function() {
   it("calculate the summ of two numbers", function() {
-    assert.equal(calculateNumber(1, 3, 'SUM'), 4);
-    assert.equal(calculateNumber(1, 3.7, 'SUM'), 5);
-    assert.equal(calculateNumber(1.2, 3.7, 'SUBTRACT'), -3);
-    assert.equal(calculateNumber(3.7, 1.5, 'DIVIDE'), 2);
-    assert.equal(calculateNumber(1.5, 0.2, 'DIVIDE'), 'Error');
-    assert.equal(calculateNumber(-4.9, 3.7, 'SUM'), -1);
-    assert.equal(calculateNumber(1.5, 3, 'SUM'), 5);
-    assert.equal(calculateNumber(1.5, -3, 'SUM'), -1);
+    assert.equal(calculateNumber('SUM', 1, 3), 4);
+    assert.equal(calculateNumber('SUM', 1, 3.7), 5);
+    assert.equal(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
+    assert.equal(calculateNumber('DIVIDE', 3.7, 1.5), 2);
+    assert.equal(calculateNumber('DIVIDE', 1.5, 0.2), 'Error');
+    assert.equal(calculateNumber('SUM', -4.9, 3.7), -1);
+    assert.equal(calculateNumber('SUM', 1.5, 3), 5);
+    assert.equal(calculateNumber('SUM', 1.5, -3), -1);
   });
 });
