fafa140748ba2e4c9872f3e7f02a4a48ed643fa3
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-24 23:14:21 +0100

    Update 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -6,7 +6,7 @@ describe("calculateNumber", function() {
     assert.equal(calculateNumber(1, 3, 'SUM'), 4);
     assert.equal(calculateNumber(1, 3.7, 'SUM'), 5);
     assert.equal(calculateNumber(1.2, 3.7, 'SUBTRACT'), -3);
-    assert.equal(calculateNumber(1.5, 3.7, 'DIVIDE'), 1);
+    assert.equal(calculateNumber(3.7, 1.5, 'DIVIDE'), 1);
     assert.equal(calculateNumber(1.5, 0.2, 'DIVIDE'), 'Error');
     assert.equal(calculateNumber(-4.9, 3.7, 'SUM'), -1);
     assert.equal(calculateNumber(1.5, 3, 'SUM'), 5);
