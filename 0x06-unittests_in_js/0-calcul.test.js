0c04295eda19b9244705b93a97d123688dc6ed7e
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-24 21:59:53 +0100

    Create 0-calcul.test.js

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,16 @@
+const assert = require("assert");
+const calculateNumber = require("./0-calcul.js");
+
+describe("calculateNumber", function() {
+  it("calculate the summ of two numbers", function() {
+    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(1.5, 3.4), 5);
+    assert.equal(calculateNumber(-4.9, 3.7), -1);
+    assert.equal(calculateNumber(1.5, 3), 5);
+    assert.equal(calculateNumber(1.5, -3), -1);
+    assert.equal(calculateNumber(-1.5, 3.7), 2);
+  });
+});
