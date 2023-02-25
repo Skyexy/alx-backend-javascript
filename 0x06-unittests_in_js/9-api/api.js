ed2f3c04c788ff439c22a6b9ed58032dbcb141de
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-25 19:56:08 +0100

    Update api.js

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -6,6 +6,10 @@ app.get('/', (rq, rs) => {
   rs.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id([0-9]+)', (rq, rs) => {
+  rs.send(`Payment methods for cart ${rq.params.id}`);
+});
+
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
