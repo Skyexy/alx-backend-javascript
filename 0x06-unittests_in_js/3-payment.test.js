96364503b0f7317f1ce6de4c5481c4a5d485e874
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-25 16:47:57 +0100

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,16 @@
 const sinon = require("sinon");
 const Utils = require("./utils");
 const sendPaymentRequestToApi = require("./3-payment");
-const assert = require("assert");
+const chai = require("chai");
+const expect = chai.expect;
 
 describe("utils", function() {
   describe("clculatenum", function(){
     it('should call save once', function() {
       var Spy = sinon.spy(Utils.calculateNumber);
       sendPaymentRequestToApi(100, 20);
-      sinon.assert.calledOnce(Spy);
-      var args = Spy.getCalls()[0].args;
-       assert.equal(args, 'SUM');
+      expect(Spy.calledOnce).to.be.true;
+      expect(res.send.firstCall.args[0]).to.equal('SUM');
     });
   });
 });
