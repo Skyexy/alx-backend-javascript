8faf54379ddbc207240b39b1a351b40e66209271
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-25 21:42:09 +0100

    Update api.test.js

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 const request = require('request');
 
 describe('test the API', () => {
-  it('test the API with status, body', (done) => {
+  it('test the API with localost:7865', (done) => {
     request('http://localhost:7865', 'GET', (er, rs, bd) => {
       if (er) throw er;
       expect(rs.statusCode).to.equal(200);
@@ -11,4 +11,56 @@ describe('test the API', () => {
     });
     done();
   });
+
+  it('test the API with cart/3', (done) => {
+    request('http://localhost:7865/cart/3', 'GET', (er, rs, bd) => {
+      if (er) throw er;
+      expect(rs.statusCode).to.equal(200);
+      expect(bd).to.equal('Payment methods for cart 3');
+    });
+    done();
+  });
+
+  it('test the API with cart/234', (done) => {
+    request('http://localhost:7865/cart/234', 'GET', (er, rs, bd) => {
+      if (er) throw er;
+      expect(rs.statusCode).to.equal(200);
+      expect(bd).to.equal('Payment methods for cart 234');
+    });
+    done();
+  });
+
+  it('test the API with cart/abc', (done) => {
+    request('http://localhost:7865/cart/abc', 'GET', (er, rs) => {
+      if (er) throw er;
+      expect(rs.statusCode).to.equal(404);
+    });
+    done();
+  });
+
+  it('test the API with cart/2bc', (done) => {
+    request('http://localhost:7865/cart/2bc', 'GET', (er, rs) => {
+      if (er) throw er;
+      expect(rs.statusCode).to.equal(404);
+    });
+    done();
+  });
+
+  it('test the API with cart/23a', (done) => {
+    request('http://localhost:7865/cart/23a', 'GET', (er, rs) => {
+      if (er) throw er;
+      expect(rs.statusCode).to.equal(404);
+    });
+    done();
+  });
+
+  it('test the API with cart', (done) => {
+    request('http://localhost:7865/cart', 'GET', (er, rs) => {
+      if (er) throw er;
+      expect(rs.statusCode).to.equal(404);
+    });
+    done();
+  });
+
+
 });
