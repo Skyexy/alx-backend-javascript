02fd5ce8d9fc14e1a0fd258f35cbedf1a710304c
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-25 23:02:31 +0100

    Update api.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,109 +1,35 @@
-const chai = require('chai');
-const expect = chai.expect;
-const request = require('request');
+const express = require('express');
+const bdparser = require('body-parser')
+const app = express();
+app.use(bdparser.urlencoded({ extended: true }));
+app.use(bdparser.json());
+app.use(bdparser.raw());
+const port = 7865;
 
-describe('test the API', () => {
-  it('test the API with localost:7865', (done) => {
-    request('http://localhost:7865', 'GET', (er, rs, bd) => {
-      if (er) throw er;
-      expect(rs.statusCode).to.equal(200);
-      expect(bd).to.equal('Welcome to the payment system');
-    });
-    done();
-  });
-
-  it('test the API with cart/3', (done) => {
-    request('http://localhost:7865/cart/3', 'GET', (er, rs, bd) => {
-      if (er) throw er;
-      expect(rs.statusCode).to.equal(200);
-      expect(bd).to.equal('Payment methods for cart 3');
-    });
-    done();
-  });
-
-  it('test the API with cart/234', (done) => {
-    request('http://localhost:7865/cart/234', 'GET', (er, rs, bd) => {
-      if (er) throw er;
-      expect(rs.statusCode).to.equal(200);
-      expect(bd).to.equal('Payment methods for cart 234');
-    });
-    done();
-  });
-
-  it('test the API with cart/abc', (done) => {
-    request('http://localhost:7865/cart/abc', 'GET', (er, rs) => {
-      if (er) throw er;
-      expect(rs.statusCode).to.equal(404);
-    });
-    done();
-  });
-
-  it('test the API with cart/2bc', (done) => {
-    request('http://localhost:7865/cart/2bc', 'GET', (er, rs) => {
-      if (er) throw er;
-      expect(rs.statusCode).to.equal(404);
-    });
-    done();
-  });
-
-  it('test the API with cart/23a', (done) => {
-    request('http://localhost:7865/cart/23a', 'GET', (er, rs) => {
-      if (er) throw er;
-      expect(rs.statusCode).to.equal(404);
-    });
-    done();
-  });
-
-  it('test the API with cart', (done) => {
-    request('http://localhost:7865/cart', 'GET', (er, rs) => {
-      if (er) throw er;
-      expect(rs.statusCode).to.equal(404);
-    });
-    done();
-  });
-
-  it('test the API /login with some body', (done) => {
-    const prm = {
-      url: 'http://localhost:7865/login',
-      method: 'POST',
-      json: {
-        userName: 'Holberton',
-      },
-    };
-    request(prm, (er, rs, bd) => {
-      if (er) throw er;
-      expect(rs.statusCode).to.equal(200);
-      expect(bd).to.equal('Welcome Holberton');
-    });
-    done();
-  });
-
-  it('test the API /login with no body', (done) => {
-    const prm = {
-      url: 'http://localhost:7865/login',
-      method: 'POST',
-      
-    };
-    request(prm, (er, rs, bd) => {
-      if (er) throw er;
-      expect(rs.statusCode).to.equal(200);
-      expect(bd).to.equal('Welcome undefined');
-    });
-    done();
-  });
-
-  it('test the API /available_payments', (done) => {
-    const prm = {
-      url: 'http://localhost:7865/available_payments',
-      method: 'GET',
-    };
-    request(prm, (er, rs, bd) => {
-      if (er) throw er;
-      expect(rs.statusCode).to.equal(200);
-      expect(bd).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
-    });
-    done();
-  });
-
-
+app.get('/', (rq, rs) => {
+  rs.send('Welcome to the payment system');
 });
+
+app.get('/cart/:id([0-9]+)', (rq, rs) => {
+  rs.send(`Payment methods for cart ${rq.params.id}`);
+});
+
+app.get('/available_payments', (rq, rs) => {
+  const ob = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  }
+  rs.json(ob);
+});
+
+app.post('/login', (rq, rs) => {
+  rs.end(`Welcome ${rq.body.userName}`);
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
