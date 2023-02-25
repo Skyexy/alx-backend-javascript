4da7b1ed571a0c9170bbf4ac8989fa0989d25f64
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-25 18:47:19 +0100

    Update 5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -7,19 +7,21 @@ const expect = chai.expect;
 describe('Connection', function () {
   let console;
   
-  beforeEach('beforeeach', function (){
+  beforeEach(() => {
     console = sinon.spy(console, 'log');
   });
-  afterEach('aftereach', function () {
+  afterEach(() => {
     console.restore();
   });
-  it('should call 120', function() {
+  it('should call 120', () => {
     sendPaymentRequestToApi(100, 20);
+    
     expect(console.calledWithExactly('The total is: 120'));
     expect(consolespy.CalledOnce);
   });
-  it('should call 20', function() {
+  it('should call 20', () => {
     sendPaymentRequestToApi(10, 20);
+    
     expect(console.calledWithExactly('The total is: 20'));
     expect(consolespy.CalledOnce);
   });
