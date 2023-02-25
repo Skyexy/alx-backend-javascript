4f7f97e5883207df50aeba0255af5d50cff8b3ec
Skyexy <99495685+Skyexy@users.noreply.github.com>
2023-02-25 17:36:39 +0100

    Update 3-payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -3,6 +3,7 @@ const Utils = require("./utils");
 function sendPaymentRequestToApi(totalAmount, totalShipping){
     total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
     console.log(`The total is: ${total}`);
+    return total;
 }
 
 module.exports = sendPaymentRequestToApi
